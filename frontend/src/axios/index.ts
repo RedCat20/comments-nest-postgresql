2b2b8c06e158b6764770de03ffbd363e698e54fa
RedCat20 <sunny.red.cat20@gmail.com>
2023-01-26 09:09:46 +0200

    create env variables for frontend

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const PORT = process.env.POSTGRES_HOST || 5000;
+const HOST = process.env.POSTGRES_HOST || 'localhost';
+
 export const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: `http://${HOST}:${PORT}`
 });
 
 export { FilesApi } from './FilesApi';
