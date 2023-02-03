e138a0c2edd966c4b632275a24e20379a723b35b
RedCat20 <sunny.red.cat20@gmail.com>
2023-02-03 15:56:00 +0200

    version without base url

--- frontend/src/axios/CommentApi.ts
+++ frontend/src/axios/CommentApi.ts
@@ -5,37 +5,37 @@ export const CommentApi = {
 
     async getMainComments(currentPage: number | string, sort: string) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.get(`${baseUrl}/comments/?page=${currentPage || 1}&sort=${sort}`);
+        const { data } = await instance.get(`/comments/?page=${currentPage || 1}&sort=${sort}`);
         return data;
     },
 
     async addComment(dto: CreateCommentDto) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.post(`${baseUrl}/comments`, dto);
+        const { data } = await instance.post(`/comments`, dto);
         return data;
     },
 
     async getOneComment(id: string) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.get(`${baseUrl}/comments/${id}`);
+        const { data } = await instance.get(`/comments/${id}`);
         return data;
     },
 
     async getCommentAnswers(id: string) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.get(`${baseUrl}/comments/${id}/answers`);
+        const { data } = await instance.get(`/comments/${id}/answers`);
         return data;
     },
 
     async getAllComments(currentPage: number | string, sort: string) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.get(`${baseUrl}/comments/all?page=${currentPage || 1}&sort=${sort}`);
+        const { data } = await instance.get(`/comments/all?page=${currentPage || 1}&sort=${sort}`);
         return data;
     },
 
     async updateComment(dto: CreateCommentDto, id: number) {
         const baseUrl = getBaseUrl();
-        const { data } = await instance.patch(`${baseUrl}/comments/${id}`, dto);
+        const { data } = await instance.patch(`/comments/${id}`, dto);
         return data;
     }
 };
