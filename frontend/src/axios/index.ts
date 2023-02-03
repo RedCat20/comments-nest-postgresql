33d830698354237ddbd364d625dc73d4b692281e
RedCat20 <sunny.red.cat20@gmail.com>
2023-02-03 14:36:55 +0200

    fix base url

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
 const PORT = process.env.POSTGRES_PORT || 5000;
-const HOST = process.env.POSTGRES_HOST || 'localhost';
+const HOST = process.env.POSTGRES_HOST || 'http://localhost';
 
 export const getBaseUrl = () => {
+    console.log('PORT: ', PORT)
+    console.log('HOST: ', HOST)
     return`${HOST}:${PORT}`;
 }
 
 export const instance = axios.create({
-    baseURL: `${HOST}:${PORT}`
+   // baseURL: getBaseUrl()
 });
 
 export { FilesApi } from './FilesApi';
